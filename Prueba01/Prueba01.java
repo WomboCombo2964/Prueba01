256522aee9e3e82c86101edfeeb9833541c026ab
WomboCombo2964 <quiquediaz.cas@gmail.com>
2023-11-28 15:05:32 +0100

    Commit Prueba1

---
+++ Prueba01/Prueba01.java
@@ -0,0 +1,9 @@
+package Prueba01;
+
+public class Prueba01 {
+    public static void main(String[] args) {
+
+        System.out.println("Prueba01");
+
+    }
+}
--- Prueba02/Prueba02.java
+++
@@ -1,7 +0,0 @@
-package Prueba02;
-
-public class Prueba02 {
-    public static void main(String[] args) {
-        System.out.println("Prueba03");
-    }
-}
